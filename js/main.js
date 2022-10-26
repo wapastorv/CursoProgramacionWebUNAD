e3f999e1c5a2fca4587d5d6da52ed6d87acfb5f6
wapastorv <williampastor@outlook.com>
2022-10-25 22:05:43 -0500

    Ajuste en el formulario de conversion de unidades y diseño de codigo de javascript para conversion de unidades

--- js/main.js
+++ js/main.js
@@ -5,10 +5,43 @@ function convertirUnidades(){
     var x = document.formularioConversionUnidades.inputGroupSelect02.value;
     var r = document.formularioConversionUnidades.inputResuldado.value;
 
-    r=((parseInt(a)*parseInt(x))/parseInt(b)) * parseInt(b);
+    if (b == "Byte" && x == "Kilobyte" || b == "Kilobyte" && x == "Megabyte" || b == "Megabyte" && x == "Gigabyte" || b == "Gigabyte" && x =="Terabyte"){
+        r = (parseFloat(a) / 1e+3);
+        document.formularioConversionUnidades.inputResuldado.value=r;
+    }
+    if(b == "Kilobyte" && x == "Byte" || b == "Megabyte" && x == "Kilobyte" || b == "Gigabyte" && x == "Megabyte" || b == "Terabyte" && x == "Gigabyte" ){
+        r = (parseFloat(a) * 1e+3);
+        document.formularioConversionUnidades.inputResuldado.value=r;
+    }
+    if(b == "Byte" && x == "Megabyte" || b == "Megabyte" && x == "Byte" || b == "Gigabyte" && x == "Kilobyte" || b == "Terabyte" && x == "Megabyte"){
+        r = (parseFloat(a)* 1e+6);
+        document.formularioConversionUnidades.inputResuldado.value=r;
+    }
+    if(b == "Kilobyte" && x == "Gigabyte" || b == "Megabyte" && x == "Terabyte"){
+        r = (parseFloat(a) / 1e+6);
+        document.formularioConversionUnidades.inputResuldado.value=r;
+    }
+    if(b == "Byte" && x == "Gigabyte" || b == "Kilobyte" && x == "Terabyte"){
+        r = (parseFloat(a) / 1e+9);
+        document.formularioConversionUnidades.inputResuldado.value=r;
+    }
+    if(b == "Gigabyte" && x == "Byte" || b == "Terabyte" && x == "Kilobyte"){
+        r = (parseFloat(a) * 1e+9);
+        document.formularioConversionUnidades.inputResuldado.value=r;
+    }
+    if(b == "Terabyte" && x == "Byte"){
+        r = (parseFloat(a)* 1e+12);
+        document.formularioConversionUnidades.inputResuldado.value=r;
+    }
+    if(b == "Byte" && x == "Terabyte"){
+        r = (parseFloat(a) / 1e+12);
+        document.formularioConversionUnidades.inputResuldado.value=r;
+    }
+
+    /*r=((parseInt(a)*parseInt(x))/parseInt(b)) * parseInt(b);
         document.formularioConversionUnidades.inputResuldado.value = r;
 
-    /* r=(parseFloat(a)*parseFloat(x))/parseFloat(b);
+    r=(parseFloat(a)*parseFloat(x))/parseFloat(b);
    document.f1.c.value=r;*/
     
 }
@@ -17,8 +50,6 @@ function calcularVenta()
     var valorCompra =   document.formVentas.valor_compra.value;
     var porcentajeUtilidad  =   document.formVentas.porcentaje_utilidad.value;
     var porcentajeImpuestos =   document.formVentas.porcentaje_impuestos.value;
-    var resultadoValorVenta =   document.formVentas.resultado_valor_venta.value;
-    var resultadoGanancias  =   document.formVentas.resultado_ganancias.value;
     var porcentaje_impuestos    =   (parseFloat(valorCompra)*parseFloat(porcentajeImpuestos))/100;
     var porcentaje_utilidad =   (parseFloat(valorCompra)*parseFloat(porcentajeUtilidad))/100;
     var r1 = parseFloat(valorCompra)+parseFloat(porcentaje_impuestos)+parseFloat(porcentaje_utilidad);
